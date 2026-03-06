150678ba78b2fac4bc4657e0414ef8404dd0c5f9
SergWebAcceptance <serg.web@acceptance.ltd>
2026-03-06 10:55:43 +0200

    Update website links in constants file to reflect new app domain

--- src/shared/lib/constants/constants.ts
+++ src/shared/lib/constants/constants.ts
@@ -3,8 +3,8 @@ export const CACHE_TAG_POLICIES = 'policies';
 export const WEBSITE_EMAIL = 'support@looniebit.com';
 
 export const LINKS = {
-  SIGN_IN: 'https://exchange.looniebit.com/login',
-  SIGN_UP: 'https://exchange.looniebit.com/register',
+  SIGN_IN: 'https://app.looniebit.com/login',
+  SIGN_UP: 'https://app.looniebit.com/register',
   SUPPORT: 'https://ticket.looniebit.com/en',
   FAQ: 'https://ticket.looniebit.com/en/knowledgebase',
   FACEBOOK: 'https://www.facebook.com/looniebit ',
